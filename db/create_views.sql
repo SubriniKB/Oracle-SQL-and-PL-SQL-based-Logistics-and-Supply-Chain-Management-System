01ac84bc726abf13c53c2a9a92d3a169711ca626
subrini <83447676+SubriniKB@users.noreply.github.com>
2026-08-09 05:56:46 +0530

    updated

---
+++ db/create_views.sql
@@ -0,0 +1,18 @@
+Oracle-SQL-and-PL-SQL-based-Logistics-and-Supply-Chain-Management-System/
+│
+├── sql/
+│   ├── 01_tables.sql
+│   ├── 02_constraints.sql
+│   ├── 03_sequences.sql
+│   ├── 04_indexes.sql
+│   ├── 05_views.sql
+│   ├── 06_          ← NEXT
+│   ├── 07_
+│   ├── 08_
+│   ├── 09_triggers.sql
+│   ├── 10_packages.sql
+│   ├── 11_reports.sql
+│   └── 12_audit.sql
+│
+└── README.md
+
